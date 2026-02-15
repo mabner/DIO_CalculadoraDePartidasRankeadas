fb24d33e5372dbc63a0d2503247f45dc16f9c277
Marcos Leite <30819867+mabner@users.noreply.github.com>
2026-02-14 23:11:57 -0300

    Using function to determine the hero level

---
+++ index_const_function.js
@@ -0,0 +1,77 @@
+/**
+ ## O Que deve ser utilizado
+
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+- Funções
+
+ ## Objetivo
+
+Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
+depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+
+Se vitórias for menor do que 10 = Ferro
+Se vitórias for entre 11 e 20 = Bronze
+Se vitórias for entre 21 e 50 = Prata
+Se vitórias for entre 51 e 80 = Ouro
+Se vitórias for entre 81 e 90 = Diamante
+Se vitórias for entre 91 e 100= Lendário
+Se vitórias for maior ou igual a 101 = Imortal
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+"O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
+ */
+
+const readline = require('readline/promises');
+const { stdin: input, stdout: output } = require('process');
+
+function calcularRankeadas(vitorias, derrotas) {
+  return vitorias - derrotas;
+}
+
+const niveis = [
+  { limite: 10, nome: 'Ferro' },
+  { limite: 20, nome: 'Bronze' },
+  { limite: 50, nome: 'Prata' },
+  { limite: 80, nome: 'Ouro' },
+  { limite: 90, nome: 'Diamante' },
+  { limite: 100, nome: 'Lendário' },
+];
+
+function determinarNivel(saldo) {
+  for (const nivel of niveis) {
+    if (saldo <= nivel.limite) {
+      return nivel.nome;
+    }
+  }
+  return 'Imortal';
+}
+
+async function executar() {
+  const rl = readline.createInterface({ input, output });
+
+  console.log('...::: Calculadora de partidas Rankeadas :::...\n');
+
+  const vitorias = Number(
+    await rl.question('Informe a quantidade de vitórias: '),
+  );
+  const derrotas = Number(
+    await rl.question('Informe a quantidade de derrotas: '),
+  );
+
+  const saldo = calcularRankeadas(vitorias, derrotas);
+  //const nivel = determinarNivel(saldo); // ?
+  const nivel = determinarNivel(vitorias);
+
+  console.log(`O Herói tem saldo de ${saldo} e está no nível de ${nivel}`);
+
+  console.log();
+
+  rl.close();
+}
+
+executar();
