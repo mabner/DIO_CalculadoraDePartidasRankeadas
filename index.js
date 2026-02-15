e75d7d0bffc69fa0cef6bae52b0c2816c1e308f1
Marcos Leite <30819867+mabner@users.noreply.github.com>
2026-02-14 22:08:43 -0300

    determinarNivel function

--- index.js
+++ index.js
@@ -31,3 +31,32 @@ function calcularRankeadas(vitorias, derrotas) {
   const saldoVitorias = vitorias - derrotas;
   return saldoVitorias;
 }
+
+function determinarNivel(saldoVitorias) {
+  let nivel = '';
+
+  switch (true) {
+    case saldoVitorias <= 10:
+      nivel = 'Ferro';
+      break;
+    case saldoVitorias > 10 && saldoVitorias <= 20:
+      nivel = 'Bronze';
+      break;
+    case saldoVitorias > 20 && saldoVitorias <= 50:
+      nivel = 'Prata';
+      break;
+    case saldoVitorias > 50 && saldoVitorias <= 80:
+      nivel = 'Ouro';
+      break;
+    case saldoVitorias > 80 && saldoVitorias <= 90:
+      nivel = 'Diamante';
+      break;
+    case saldoVitorias > 90 && saldoVitorias <= 100:
+      nivel = 'Lendário';
+      break;
+    case saldoVitorias >= 101:
+      nivel = 'Imortal';
+      break;
+  }
+  return nivel;
+}
